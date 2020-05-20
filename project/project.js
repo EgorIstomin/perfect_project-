23526bc6099c0b9214ecc75f69bd4dc7f473a3b1
EgorIstomin <60170293+EgorIstomin@users.noreply.github.com>
2020-05-20 12:11:06 +0500

    Add files via upload

---
+++ project/project.js
@@ -0,0 +1,7 @@
+let block1 = document.querySelector("#time_block1"); 
+let career_img1 = document.querySelector("#career_img1")
+
+time_block1.addEventListener('mouseenter', function() { 
+    career_img1.classList.toggle('dark');
+  });
+  
